8c5e06f020149844d6c315bc4f19e9b4cb0462fe
Marcelo Eltz <marcelo@growdev.com.br>
2020-07-11 08:50:46 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:simple_form_flutter/screens/form_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Formulário de çadastro',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FormPage(),
+    );
+  }
+}
